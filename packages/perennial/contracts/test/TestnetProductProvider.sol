65abf17ee6a00836fa17d65fec031d327b386caf
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2022-06-24 15:36:56 -0400

    [Fix] I03 - Floating pragma allows use of buggy compiler versions.

--- packages/perennial/contracts/test/TestnetProductProvider.sol
+++ packages/perennial/contracts/test/TestnetProductProvider.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.14;
+pragma solidity 0.8.15;
 
 import "@equilibria/root/curve/types/JumpRateUtilizationCurve.sol";
 import "../interfaces/IProductProvider.sol";
