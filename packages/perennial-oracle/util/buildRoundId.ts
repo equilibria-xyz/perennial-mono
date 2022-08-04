237231afc723ac1f41e8f3647bbad681735afbf3
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2022-08-04 11:52:49 -0400

    working package

--- packages/perennial-oracle/util/buildRoundId.ts
+++ packages/perennial-oracle/util/buildRoundId.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from 'ethers'
+import { BigNumber } from '@ethersproject/bignumber'
 
 export function buildRoundId(phaseId: number, aggregatorRoundId: number): BigNumber {
   return BigNumber.from(16).pow(16).mul(phaseId).add(aggregatorRoundId)
