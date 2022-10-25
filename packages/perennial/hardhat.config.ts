a48fbc9865da4d3dcbf405de0b916943474eaedd
arjun-io <2940142+arjun-io@users.noreply.github.com>
2022-10-25 10:40:15 -0400

    Minimum Contract size, lint fixes, timeouts (#78)
    * Minimum Contract size, lint fixes, timeouts
    
    * clarify storageLayout config in hardhat

--- packages/perennial/hardhat.config.ts
+++ packages/perennial/hardhat.config.ts
@@ -3,24 +3,45 @@ import { dirname } from 'path'
 import defaultConfig, { OPTIMIZER_ENABLED, SOLIDITY_VERSION } from '../common/hardhat.default.config'
 const eqPerennialOracleDir = dirname(require.resolve('@equilibria/perennial-oracle/package.json'))
 
+// This Solidity config produces small contract sizes, and is useful when
+// contracts are close to the maximum possible size. The trade off is each
+// function call will likely use extra gas.
+// Mostly inspired by Compound Comet's setup: https://github.com/compound-finance/comet/blob/main/hardhat.config.ts#L124
+const MINIMUM_CONTRACT_SIZE_SOLIDITY_OVERRIDES = {
+  version: SOLIDITY_VERSION,
+  settings: {
+    viaIR: OPTIMIZER_ENABLED,
+    optimizer: OPTIMIZER_ENABLED
+      ? {
+          enabled: true,
+          runs: 1,
+          details: {
+            yulDetails: {
+              // We checked with the Compound team to confirm that this should be safe to use to other projects
+              optimizerSteps:
+                'dhfoDgvulfnTUtnIf [xa[r]scLM cCTUtTOntnfDIul Lcul Vcul [j] Tpeul xa[rul] xa[r]cL gvif CTUca[r]LsTOtfDnca[r]Iulc] jmul[jul] VcTOcul jmul',
+            },
+          },
+        }
+      : { enabled: false },
+    outputSelection: OPTIMIZER_ENABLED
+      ? {
+          '*': {
+            '*': ['evm.deployedBytecode.sourceMap'],
+          },
+        }
+      : {
+          '*': {
+            '*': ['storageLayout'], // This is needed by Smock for mocking functions
+          },
+        },
+  },
+}
+
 const config = defaultConfig({
   solidityOverrides: {
-    'contracts/product/Product.sol': {
-      version: SOLIDITY_VERSION,
-      settings: {
-        optimizer: {
-          enabled: OPTIMIZER_ENABLED,
-          runs: 5800, // Maximum value as of commit e6b7ab7
-        },
-        outputSelection: OPTIMIZER_ENABLED
-          ? {}
-          : {
-              '*': {
-                '*': ['storageLayout'],
-              },
-            },
-      },
-    },
+    'contracts/product/Product.sol': MINIMUM_CONTRACT_SIZE_SOLIDITY_OVERRIDES,
+    'contracts/lens/PerennialLens.sol': MINIMUM_CONTRACT_SIZE_SOLIDITY_OVERRIDES,
   },
   externalDeployments: {
     kovan: [`${eqPerennialOracleDir}/deployments/kovan`],
