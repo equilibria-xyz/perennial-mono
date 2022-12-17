d5ebf8acee7b4489732015c4822fb4685157e3ad
Kevin Britz <kevin@equilibria.xyz>
2022-12-16 23:43:47 -0800

    some cleanup

--- packages/perennial-vaults/contracts/PerennialBalancedVault.sol
+++ packages/perennial-vaults/contracts/PerennialBalancedVault.sol
@@ -12,6 +12,8 @@ import "@equilibria/root/control/unstructured/UOwnable.sol";
 //TODO: pausable
 //TODO: gas is gonna be insane
 contract PerennialBalancedVault is ERC4626, UOwnable {
+    UFixed18 constant private TWO = UFixed18.wrap(2e18);
+
     IController public immutable controller;
     IProduct public immutable long;
     IProduct public immutable short;
@@ -47,9 +49,7 @@ contract PerennialBalancedVault is ERC4626, UOwnable {
     // Precondition: Assumes the collateral is balanced and the positions have equal size.
     function maxWithdraw(address owner) public view virtual override returns (uint256) {
         // If we're in the middle of closing all positions due to liquidations, return 0.
-        if (_hasOutstandingLiquidation()) {
-            return 0;
-        }
+        if (_hasOutstandingLiquidation()) return 0;
 
         // Calculate the minimum amount of collateral we can have.
         IOracleProvider.OracleVersion memory currentOracleVersion = long.atVersion(long.latestVersion());
@@ -58,7 +58,7 @@ contract PerennialBalancedVault is ERC4626, UOwnable {
 
         // Calculate the minimum collateral for one product, which represents having a leverage of `leverage` * `maxLeverageMultiplier`.
         UFixed18 minimumCollateralForOneProduct = position.mul(price).div(leverage.mul(maxLeverageMultiplier));
-        UFixed18 minimumCollateral = minimumCollateralForOneProduct.mul(UFixed18Lib.from(2));
+        UFixed18 minimumCollateral = minimumCollateralForOneProduct.mul(TWO);
 
         UFixed18 currentCollateral = collateral.collateral(address(this), long).add(collateral.collateral(address(this), short));
         if (currentCollateral.lt(minimumCollateral)) {
@@ -71,78 +71,41 @@ contract PerennialBalancedVault is ERC4626, UOwnable {
 
     // One can call this function with eth_call to get the max withdraw after the flywheel has run.
     function maxWithdrawWithSettle(address owner) public returns (uint256) {
-        _flywheel();
+        _before();
         return maxWithdraw(owner);
     }
 
     // TODO: how do we incentivize / keeper this so that it gets called to rebalance the vault before it gets liquidated?
     // TODO: most of the time user withdraw / deposits will be enough, but a large direction movement in-between user actions could cause this to get liquidated
     function sync() external {
-        _flywheel();
+        _before();
+        _updateCollateral(UFixed18Lib.ZERO, false);
+        _updatePosition();
     }
 
     function deposit(uint256 assets, address receiver) public virtual override returns (uint256) {
-        require(_flywheel(), "vault needs to recover from liquidation before resuming");
+        _before();
         return super.deposit(assets, receiver);
     }
 
     function mint(uint256 shares, address receiver) public virtual override returns (uint256) {
-        require(_flywheel(), "vault needs to recover from liquidation before resuming");
+        _before();
         return super.mint(shares, receiver);
     }
 
     function withdraw(uint256 assets, address receiver, address owner) public virtual override returns (uint256) {
-        require(_flywheel(), "vault needs to recover from liquidation before resuming");
+        _before();
         return super.withdraw(assets, receiver, owner);
     }
 
     function redeem(uint256 shares, address receiver, address owner) public virtual override returns (uint256) {
-        require(_flywheel(), "vault needs to recover from liquidation before resuming");
+        _before();
         return super.redeem(shares, receiver, owner);
     }
 
-    // Returns whether the vault is in a healthy state (i.e. if it is not recovering from liquidations).
-    function _flywheel() private returns (bool) {
-        // Force settlement.
+    function _before() private {
         long.settleAccount(address(this));
         short.settleAccount(address(this));
-
-        // If positions are are not equal, a liquidation occurred. Close all positions so that the flywheel can rebalance.
-        if (_hasOutstandingLiquidation()) {
-            _closeAllPositions();
-        }
-
-        // Adjust collateral to be equal.
-        UFixed18 longCollateral = collateral.collateral(address(this), long);
-        UFixed18 shortCollateral = collateral.collateral(address(this), short);
-        (
-            IProduct withdrawFrom,
-            UFixed18 withdrawCollateral,
-            IProduct depositTo,
-            UFixed18 depositCollateral
-        ) = longCollateral.gt(shortCollateral) ?
-        (
-            long,
-            longCollateral,
-            short,
-            shortCollateral
-        ) :
-        (
-            short,
-            shortCollateral,
-            long,
-            longCollateral
-        );
-        if (UFixed18.unwrap(withdrawCollateral.sub(depositCollateral)) > 1) {
-            UFixed18 adjustmentAmount = withdrawCollateral.sub(depositCollateral).div(UFixed18Lib.from(2));
-            collateral.withdrawTo(address(this), withdrawFrom, adjustmentAmount);
-            collateral.depositTo(address(this), depositTo, adjustmentAmount);
-        }
-
-        // Rebalance position.
-        _rebalancePosition();
-
-        return true;
     }
 
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
@@ -150,19 +113,18 @@ contract PerennialBalancedVault is ERC4626, UOwnable {
         super._deposit(caller, receiver, assets, shares);
 
         // Deposit assets into collateral.
-        //TODO: assumes equivalent maintenance on the two products?
-        _rebalanceCollateral(UFixed18.wrap(assets), true);
+        _updateCollateral(UFixed18.wrap(assets), true);
 
         // Open positions to maintain leverage.
-        _rebalancePosition();
+        _updatePosition();
     }
 
     function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal override {
         // Withdraw assets from this vault.
-        _rebalanceCollateral(UFixed18.wrap(assets), false);
+        _updateCollateral(UFixed18.wrap(assets), false);
 
         // Close positions to maintain leverage.
-        _rebalancePosition();
+        _updatePosition();
 
         // Make Withdrawal
         super._withdraw(caller, receiver, owner, assets, shares);
@@ -171,45 +133,36 @@ contract PerennialBalancedVault is ERC4626, UOwnable {
     // If `deposit` is true, deposit a total of `amount` collateral into the two products.
     // If `deposit` is false, withdraw a total of `amount` collateral from the two products.
     // Precondition: long and short either have the same amount of collateral or the difference is 1.
-    function _rebalanceCollateral(UFixed18 amount, bool isDeposit) internal {
+    function _updateCollateral(UFixed18 amount, bool isDeposit) internal {
         UFixed18 longCollateral = collateral.collateral(address(this), long);
         UFixed18 shortCollateral = collateral.collateral(address(this), short);
-        (
-            IProduct moreCollateral,
-            IProduct lessCollateral
-        ) = longCollateral.gt(shortCollateral) ?
-        (
-            long,
-            short
-        ) :
-        (
-            short,
-            long
-        );
+        UFixed18 totalCollateral = longCollateral.add(shortCollateral);
 
-        UFixed18 amountSmallerHalf = UFixed18.wrap(UFixed18.unwrap(amount) / 2);
-        UFixed18 amountLargerHalf = amount.sub(amountSmallerHalf);
+        totalCollateral = isDeposit ? totalCollateral.add(amount) : totalCollateral.sub(amount);
+        UFixed18 targetCollateral = totalCollateral.div(TWO);
+        (IProduct greaterProduct, IProduct lesserProduct) = longCollateral.gt(shortCollateral) ?
+            (long, short) :
+            (short, long);
 
-        if (isDeposit) {
-            collateral.depositTo(address(this), moreCollateral, amountSmallerHalf);
-            collateral.depositTo(address(this), lessCollateral, amountLargerHalf);
-        } else {
-            collateral.withdrawTo(address(this), moreCollateral, amountLargerHalf);
-            collateral.withdrawTo(address(this), lessCollateral, amountSmallerHalf);
-        }
+        _adjustCollateral(greaterProduct, targetCollateral);
+        _adjustCollateral(lesserProduct, totalCollateral.sub(targetCollateral));
     }
 
     // Precondition: the difference in collateral between long and short is at most 1.
     // Precondition: the position sizes are the same.
-    function _rebalancePosition() internal {
-        // 1. Calculate the target position size for each product.
-        UFixed18 longCollateral = collateral.collateral(address(this), long);
-        UFixed18 shortCollateral = collateral.collateral(address(this), short);
-        UFixed18 collateralAmount = UFixed18Lib.min(longCollateral, shortCollateral);
+    function _updatePosition() internal {
+        // 0. If recently liquidated, reset positions
+        if (_hasOutstandingLiquidation()) {
+            _adjustPosition(long, UFixed18Lib.ZERO);
+            _adjustPosition(short, UFixed18Lib.ZERO);
+            return;
+        }
 
-        collateralAmount = collateralAmount.gt(fixedFloat.div(UFixed18Lib.from(2))) ? collateralAmount.sub(fixedFloat.div(UFixed18Lib.from(2))) : UFixed18Lib.ZERO;
+        // 1. Calculate the target position size for each product.
+        UFixed18 totalCollateral = UFixed18.wrap(totalAssets());
+        UFixed18 totalUtilized = totalCollateral.gt(fixedFloat) ? totalCollateral.sub(fixedFloat) : UFixed18Lib.ZERO;
         IOracleProvider.OracleVersion memory currentOracleVersion = long.atVersion(long.latestVersion());
-        UFixed18 targetPosition = collateralAmount.mul(leverage).div(currentOracleVersion.price.abs());
+        UFixed18 targetPosition = totalUtilized.mul(leverage).div(currentOracleVersion.price.abs()).div(TWO);
 
         // 2. Adjust positions to target position size.
         _adjustPosition(long, targetPosition);
@@ -217,25 +170,21 @@ contract PerennialBalancedVault is ERC4626, UOwnable {
     }
 
     function _adjustPosition(IProduct product, UFixed18 targetPosition) internal {
-        UFixed18 position = product.position(address(this)).next(product.pre(address(this))).maker;
-        if (position.gt(targetPosition)) {
-            product.closeMake(position.sub(targetPosition));
-        } else if (position.lt(targetPosition)) {
-            product.openMake(targetPosition.sub(position));
-        }
+        UFixed18 _position = product.position(address(this)).next(product.pre(address(this))).maker;
+        if (_position.gt(targetPosition)) product.closeMake(_position.sub(targetPosition));
+        if (_position.lt(targetPosition)) product.openMake(targetPosition.sub(_position));
+    }
+
+    function _adjustCollateral(IProduct product, UFixed18 targetCollateral) internal {
+        UFixed18 _collateral = collateral.collateral(address(this), product);
+        if (_collateral.gt(targetCollateral))
+            collateral.withdrawTo(address(this), product, _collateral.sub(targetCollateral));
+        if (_collateral.lt(targetCollateral))
+            collateral.depositTo(address(this), product, targetCollateral.sub(_collateral));
     }
 
     // Returns whether one of the positions has been liquidated and the vault has not recovered yet.
     function _hasOutstandingLiquidation() internal view returns (bool) {
         return !long.position(address(this)).maker.eq(short.position(address(this)).maker);
-    }
-
-    function _closeAllPositions() internal {
-        if (!long.position(address(this)).maker.isZero()) {
-            long.closeMake(long.position(address(this)).maker);
-        }
-        if (!short.position(address(this)).maker.isZero()) {
-            short.closeMake(short.position(address(this)).maker);
-        }
     }
 }
