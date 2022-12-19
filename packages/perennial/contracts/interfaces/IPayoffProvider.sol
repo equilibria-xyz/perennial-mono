5c7d653c40a0d0285bc5cda3152839904615ced1
arjun-io <2940142+arjun-io@users.noreply.github.com>
2022-12-19 16:44:20 -0500

    Bug Fix: StartingVersionForPhaseId contains incorrect values (#106)
    * Bug Fix: StartingVersionForPhaseId contains incorrect values
    
    * allow Oracle to be updated in Product
    
    * Reduce Product.sol size to get deployable (#102)
    
    * [Gas Savings] Remove var init, consolidate fee errors
    
    * [Version Fixes] Move type contracts to ^0.8.13
    
    * [Solidity Upgrade] Compile large contracts with 0.8.17 for size reduction
    
    * InvalidFeeValue -> InvalidParamValue
    
    * Gas Opt: Rework ChainlinkOracle's phase-tracking (#104)
    
    * phase-based data structure
    
    * fix nits
    
    * fix sync and tests
    
    * fix CI
    
    Co-authored-by: Kevin Britz <kevinbritz0@gmail.com>

--- packages/perennial/contracts/interfaces/IPayoffProvider.sol
+++ packages/perennial/contracts/interfaces/IPayoffProvider.sol
@@ -6,6 +6,8 @@ import "@equilibria/perennial-oracle/contracts/interfaces/IOracleProvider.sol";
 import "./types/PayoffDefinition.sol";
 
 interface IPayoffProvider {
+    event OracleUpdated(address newOracle);
+
     error PayoffProviderInvalidOracle();
     error PayoffProviderInvalidPayoffDefinitionError();
 
