2e407dcd426c4b3424b86dd8949aad6f6e255317
arjun-io <2940142+arjun-io@users.noreply.github.com>
2023-01-15 20:57:15 -0500

    Oracle Helper Tasks (#117)
    * Deploy MultiInvoker
    
    * mainnet deploy
    
    * incentivizer deploys
    
    * Add TwoWayBatcher support to MultiInvoker (#90)
    
    * Add TwoWayBatcher support to MultiInvoker
    
    * Fix TestnetBatcher and TestnetReserve
    
    * Use minCollateral in Liquidate is maintenance < minCollateral (#98)
    
    * Use minCollateral in Liquidate is maintenance < minCollateral
    
    * fix integration test
    
    * MultiInvoker: Bypass Batcher if set to 0-address (#100)
    
    * Bug Fix: StartingVersionForPhaseId contains incorrect values (#101)
    
    * Bug Fix: StartingVersionForPhaseId contains incorrect values
    
    * allow Oracle to be updated in Product
    
    * Reduce Product.sol size to get deployable (#102)
    
    * [Gas Savings] Remove var init, consolidate fee errors
    
    * [Version Fixes] Move type contracts to ^0.8.13
    
    * [Solidity Upgrade] Compile large contracts with 0.8.17 for size reduction
    
    * InvalidFeeValue -> InvalidParamValue
    
    * Gas Opt: Rework ChainlinkOracle's phase-tracking (#104)
    
    * phase-based data structure
    
    * fix nits
    
    Co-authored-by: Kevin Britz <kevinbritz0@gmail.com>
    
    * Revert "Bug Fix: StartingVersionForPhaseId contains incorrect values (#101)"
    
    This reverts commit 227f1b997e393de0783baa9e37377ac871e9df38.
    
    * Bug Fix: StartingVersionForPhaseId contains incorrect values (#106)
    
    * Bug Fix: StartingVersionForPhaseId contains incorrect values
    
    * allow Oracle to be updated in Product
    
    * Reduce Product.sol size to get deployable (#102)
    
    * [Gas Savings] Remove var init, consolidate fee errors
    
    * [Version Fixes] Move type contracts to ^0.8.13
    
    * [Solidity Upgrade] Compile large contracts with 0.8.17 for size reduction
    
    * InvalidFeeValue -> InvalidParamValue
    
    * Gas Opt: Rework ChainlinkOracle's phase-tracking (#104)
    
    * phase-based data structure
    
    * fix nits
    
    * fix sync and tests
    
    * fix CI
    
    Co-authored-by: Kevin Britz <kevinbritz0@gmail.com>
    
    * ChainlinkFeedOracle (#105)
    
    * ChainlinkFeedOracle
    
    * walkback logic
    
    * solidity fixes, add tests
    
    * fix comments, uncomment test
    
    * add package patch
    
    * Include latestVersion in updateOracle event (#107)
    
    * Include latestVersion in updateOracle event
    
    * fix CI
    
    * [Partial Fix] M05 - Explicit revert for multiple phase syncs (#110)
    
    * [Fix] N09 - Wrong Natspec comment (#111)
    
    * [Partial Fix] M05 - Explicit revert for multiple phase syncs
    
    * [Fix] N09 - Wrong Natspec comment
    
    * [Fix] M06 - Handle empty phases (#112)
    
    * [Partial Fix] M05 - Explicit revert for multiple phase syncs
    
    * [Fix] N09 - Wrong Natspec comment
    
    * [Fix] M06 - Handle empty phases
    
    * Revert if latestRoundData has roundId = 0
    
    * fix comment
    
    * Return 0 in getRoundCount for empty phases in ChainlinkAggregator (#115)
    
    * [Partial Fix] M05 - Explicit revert for multiple phase syncs
    
    * [Fix] N09 - Wrong Natspec comment
    
    * [Fix] M06 - Handle empty phases
    
    * Revert if latestRoundData has roundId = 0
    
    * fix comment
    
    * Return 0 in getRoundCount for empty phases in ChainlinkAggregator
    
    * Add enumerateChainlinkRounds and compareOracleVersions tasks
    
    * fix CI
    
    Co-authored-by: Kevin Britz <kevinbritz0@gmail.com>

--- packages/perennial-oracle/hardhat.config.ts
+++ packages/perennial-oracle/hardhat.config.ts
@@ -1,5 +1,7 @@
 import defaultConfig from '../common/hardhat.default.config'
 
+import './tasks'
+
 const config = defaultConfig({
   dependencyPaths: ['@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol'],
 })
