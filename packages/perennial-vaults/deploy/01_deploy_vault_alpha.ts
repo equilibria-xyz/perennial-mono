110878aff1104ecbc0767b449dd728c0810283d4
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-02-18 20:47:01 -0500

    Arbitrum and Arbitrum Goerli MultiInvoker + Vault deploys

---
+++ packages/perennial-vaults/deploy/01_deploy_vault_alpha.ts
@@ -0,0 +1,54 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { BalancedVault__factory } from '../types/generated'
+
+const VAULT_TOKEN_NAME = 'Perennial Vault Alpha'
+const VAULT_TOKEN_SYMBOL = 'PVA'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, ethers } = hre
+  const { deploy, get } = deployments
+  const { deployer } = await getNamedAccounts()
+  const deployerSigner: SignerWithAddress = await ethers.getSigner(deployer)
+
+  // TODO: network dependent
+  const proxyAdminAddress = (await get('ProxyAdmin')).address
+  const dsu = (await get('DSU')).address
+  const controller = (await get('Controller_Proxy')).address
+  const long = (await get('Product_LongEther')).address
+  const short = (await get('Product_ShortEther')).address
+  const targetLeverage = ethers.utils.parseEther('3')
+  const maxCollateral = ethers.utils.parseEther('1000000')
+
+  const vaultImpl = await deploy('PerennialVaultAlpha_Impl', {
+    contract: 'BalancedVault',
+    args: [dsu, controller, long, short, targetLeverage, maxCollateral],
+    from: deployer,
+    skipIfAlreadyDeployed: true,
+    log: true,
+    autoMine: true,
+  })
+
+  await deploy('PerennialVaultAlpha_Proxy', {
+    contract: 'TransparentUpgradeableProxy',
+    args: [vaultImpl.address, proxyAdminAddress, '0x'],
+    from: deployer,
+    skipIfAlreadyDeployed: true,
+    log: true,
+    autoMine: true,
+  })
+
+  // Initialize
+  const vault = new BalancedVault__factory(deployerSigner).attach((await get('PerennialVaultAlpha_Proxy')).address)
+  if ((await vault.name()) === VAULT_TOKEN_NAME) {
+    console.log('PerennialVaultAlpha already initialized.')
+  } else {
+    process.stdout.write('initializing BalancedVaultETH...')
+    await (await vault.initialize(VAULT_TOKEN_NAME, VAULT_TOKEN_SYMBOL)).wait(2)
+    process.stdout.write('complete.\n')
+  }
+}
+
+export default func
+func.tags = ['PerennialVaultAlpha']
--- packages/perennial-vaults/hardhat.config.ts
+++ packages/perennial-vaults/hardhat.config.ts
@@ -2,16 +2,20 @@ import { dirname } from 'path'
 
 import defaultConfig from '../common/hardhat.default.config'
 const eqPerennialDir = dirname(require.resolve('@equilibria/perennial/package.json'))
-const eqPerennialOracleDir = dirname(require.resolve('@equilibria/perennial-oracle/package.json'))
 
 const config = defaultConfig({
   solidityVersion: '0.8.17',
   externalDeployments: {
-    kovan: [`${eqPerennialDir}/deployments/kovan`, `${eqPerennialOracleDir}/deployments/kovan`],
-    goerli: [`${eqPerennialDir}/deployments/goerli`, `${eqPerennialOracleDir}/deployments/goerli`],
-    mainnet: [`${eqPerennialDir}/deployments/mainnet`, `${eqPerennialOracleDir}/deployments/mainnet`],
-    hardhat: [`${eqPerennialDir}/deployments/mainnet`, `${eqPerennialOracleDir}/deployments/mainnet`],
-    localhost: [`${eqPerennialDir}/deployments/localhost`, `${eqPerennialOracleDir}/deployments/localhost`],
+    kovan: [`${eqPerennialDir}/deployments/kovan`, `${eqPerennialDir}/external/deployments/kovan`],
+    goerli: [`${eqPerennialDir}/deployments/goerli`, `${eqPerennialDir}/external/deployments/goerli`],
+    mainnet: [`${eqPerennialDir}/deployments/mainnet`, `${eqPerennialDir}/external/deployments/mainnet`],
+    arbitrumGoerli: [
+      `${eqPerennialDir}/deployments/arbitrumGoerli`,
+      `${eqPerennialDir}/external/deployments/arbitrumGoerli`,
+    ],
+    arbitrum: [`${eqPerennialDir}/deployments/arbitrum`, `${eqPerennialDir}/external/deployments/arbitrum`],
+    hardhat: [`${eqPerennialDir}/deployments/mainnet`, `${eqPerennialDir}/external/deployments/mainnet`],
+    localhost: [`${eqPerennialDir}/deployments/localhost`, `${eqPerennialDir}/external/deployments/localhost`],
   },
   dependencyPaths: ['@equilibria/perennial/contracts/interfaces/IController.sol'],
 })
