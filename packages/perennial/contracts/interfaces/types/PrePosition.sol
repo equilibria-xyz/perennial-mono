d4a5514e693809db54a5dd9a63534b46ead19511
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-03-03 15:30:27 -0500

    cleanup

--- packages/perennial/contracts/interfaces/types/PrePosition.sol
+++ packages/perennial/contracts/interfaces/types/PrePosition.sol
@@ -40,10 +40,6 @@ library PrePositionLib {
         return self.openPosition.isEmpty() && self.closePosition.isEmpty();
     }
 
-    function isVersionZero(PrePosition memory self) internal pure returns (bool) {
-        return self.oracleVersion == 0;
-    }
-
     /**
      * @notice Increments the maker side of the open position delta
      * @param self The struct to operate on
@@ -99,7 +95,7 @@ library PrePositionLib {
         PrePosition memory self,
         IOracleProvider.OracleVersion memory toOracleVersion
     ) internal pure returns (bool) {
-        return !isVersionZero(self) && toOracleVersion.version > self.oracleVersion;
+        return self.oracleVersion != 0 && toOracleVersion.version > self.oracleVersion;
     }
 
     /**
