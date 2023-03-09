4f73b2c38a673ed20d4ca68c6ff460243beda927
test9955667 <79500129+test9955667@users.noreply.github.com>
2023-03-09 13:14:26 -0600

    update interface

--- packages/perennial/contracts/interfaces/IMultiInvokerRollup.sol
+++ packages/perennial/contracts/interfaces/IMultiInvokerRollup.sol
@@ -1,46 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-import "@equilibria/root/token/types/Token6.sol";
-import "@equilibria/root/token/types/Token18.sol";
-import "@equilibria/emptyset-batcher/interfaces/IBatcher.sol";
-import "@equilibria/emptyset-batcher/interfaces/IEmptySetReserve.sol";
+import "./IMultiInvoker.sol";
 
-import "./IController.sol";
-import "./ICollateral.sol";
-import "./IProduct.sol";
-
-interface IMultiInvokerRollup {
-    /// @dev Core protocol actions that can be composed
-    enum PerennialAction {
-        NO_OP,
-        DEPOSIT,
-        WITHDRAW,
-        OPEN_TAKE,
-        CLOSE_TAKE,
-        OPEN_MAKE,
-        CLOSE_MAKE,
-        CLAIM,
-        WRAP,
-        UNWRAP,
-        WRAP_AND_DEPOSIT,
-        WITHDRAW_AND_UNWRAP
-    }
-
-    /// @dev Struct for action invocation
-    struct Invocation {
-        PerennialAction action;
-        bytes args;
-    }
-
-    function initialize() external;
-    function USDC() external view returns (Token6); // solhint-disable-line func-name-mixedcase
-    function DSU() external view returns (Token18); // solhint-disable-line func-name-mixedcase
-    function batcher() external view returns (IBatcher);
-    function controller() external view returns (IController);
-    function collateral() external view returns (ICollateral);
-    function reserve() external view returns (IEmptySetReserve);
-    function invoke(Invocation[] calldata invocations) external;
-    function userNonces(address) external view returns (uint);
-    function productNonces(address) external view returns (uint);
+interface IMultiInvokerRollup is IMultiInvoker {
+    function addressNonce() external view returns(uint256);
+    function addressCache(uint256 nonce) external view returns(address);
+    function addressNonces(address addr) external view returns(uint256 nonce);
 }
