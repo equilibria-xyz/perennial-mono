969147ca0f6651aded63e837bed9488c4dbb841e
test9955667 <79500129+test9955667@users.noreply.github.com>
2023-03-09 13:27:57 -0600

    deployment

--- packages/perennial/deploy/005_deploy_multiinvoker.ts
+++ packages/perennial/deploy/005_deploy_multiinvoker.ts
@@ -2,7 +2,7 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { Deployment } from 'hardhat-deploy/dist/types'
 import { getMultisigAddress } from '../../common/testutil/constants'
-import { Controller__factory, IERC20__factory, MultiInvoker__factory } from '../types/generated'
+import { Controller__factory, IERC20__factory, MultiInvokerRollup__factory } from '../types/generated'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -35,45 +35,45 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const usdc = await IERC20__factory.connect(usdcAddress, deployerSigner)
 
   const multiInvokerImpl: Deployment = await deploy('MultiInvoker_Impl', {
-    contract: 'MultiInvoker',
+    contract: 'MultiInvokerRollup',
     args: [usdcAddress, batcherAddress, reserveAddress, controllerAddress],
     from: deployer,
-    skipIfAlreadyDeployed: true,
+    skipIfAlreadyDeployed: false,
     log: true,
     autoMine: true,
   })
 
-  await deploy('MultiInvoker_Proxy', {
-    contract: 'TransparentUpgradeableProxy',
-    args: [multiInvokerImpl.address, proxyAdminAddress, '0x'],
-    from: deployer,
-    skipIfAlreadyDeployed: true,
-    log: true,
-    autoMine: true,
-  })
+  // await deploy('MultiInvoker_Proxy', {
+  //   contract: 'TransparentUpgradeableProxy',
+  //   args: [multiInvokerImpl.address, proxyAdminAddress, '0x'],
+  //   from: deployer,
+  //   skipIfAlreadyDeployed: true,
+  //   log: true,
+  //   autoMine: true,
+  // })
 
-  // INITIALIZE
-  const multiInvoker = await new MultiInvoker__factory(deployerSigner).attach((await get('MultiInvoker_Proxy')).address)
+  // // INITIALIZE
+  // const multiInvoker = await new MultiInvoker__factory(deployerSigner).attach((await get('MultiInvoker_Proxy')).address)
 
-  if ((await usdc.callStatic.allowance(multiInvoker.address, reserveAddress)).eq(ethers.constants.MaxUint256)) {
-    console.log('MultiInvoker already initialized.')
-  } else {
-    process.stdout.write('initializing MultiInvoker... ')
-    await (await multiInvoker.initialize()).wait(2)
-    process.stdout.write('complete.\n')
-  }
+  // if ((await usdc.callStatic.allowance(multiInvoker.address, reserveAddress)).eq(ethers.constants.MaxUint256)) {
+  //   console.log('MultiInvoker already initialized.')
+  // } else {
+  //   process.stdout.write('initializing MultiInvoker... ')
+  //   await (await multiInvoker.initialize()).wait(2)
+  //   process.stdout.write('complete.\n')
+  // }
 
-  const controller = await new Controller__factory(deployerSigner).attach(controllerAddress)
+  // const controller = await new Controller__factory(deployerSigner).attach(controllerAddress)
 
-  // Set MultiInvoker on Controller
-  if ((await controller.multiInvoker()) === multiInvoker.address) {
-    console.log('MultiInvoker already set on Controller.')
-  } else {
-    process.stdout.write('Setting MultiInvoker address on Controller... ')
-    await (await controller.updateMultiInvoker(multiInvoker.address)).wait(2)
-    process.stdout.write('complete.\n')
-  }
+  // // Set MultiInvoker on Controller
+  // if ((await controller.multiInvoker()) === multiInvoker.address) {
+  //   console.log('MultiInvoker already set on Controller.')
+  // } else {
+  //   process.stdout.write('Setting MultiInvoker address on Controller... ')
+  //   await (await controller.updateMultiInvoker(multiInvoker.address)).wait(2)
+  //   process.stdout.write('complete.\n')
+  // }
 }
 
 export default func
-func.tags = ['MultiInvoker']
+func.tags = ['MultiInvokerRollup']
