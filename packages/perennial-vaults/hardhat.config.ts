bdb9067ebafb14b35db04971d5eb375797718522
Fennel <0xfennel@proton.me>
2023-03-29 16:07:35 -0700

    Quick changes to compiler settings

--- packages/perennial-vaults/hardhat.config.ts
+++ packages/perennial-vaults/hardhat.config.ts
@@ -1,10 +1,44 @@
 import { dirname } from 'path'
 
-import defaultConfig, { FORK_ENABLED, FORK_NETWORK } from '../common/hardhat.default.config'
+import defaultConfig, { OPTIMIZER_ENABLED, FORK_ENABLED, FORK_NETWORK } from '../common/hardhat.default.config'
 const eqPerennialDir = dirname(require.resolve('@equilibria/perennial/package.json'))
+
+const MINIMUM_CONTRACT_SIZE_SOLIDITY_OVERRIDES = {
+  version: '0.8.17',
+  settings: {
+    viaIR: OPTIMIZER_ENABLED,
+    optimizer: OPTIMIZER_ENABLED
+      ? {
+          enabled: true,
+          runs: 1,
+          details: {
+            yulDetails: {
+              // We checked with the Compound team to confirm that this should be safe to use to other projects
+              optimizerSteps:
+                'dhfoDgvulfnTUtnIf [xa[r]scLM cCTUtTOntnfDIul Lcul Vcul [j] Tpeul xa[rul] xa[r]cL gvif CTUca[r]LsTOtfDnca[r]Iulc] jmul[jul] VcTOcul jmul',
+            },
+          },
+        }
+      : { enabled: false },
+    outputSelection: OPTIMIZER_ENABLED
+      ? {
+          '*': {
+            '*': ['evm.deployedBytecode.sourceMap'],
+          },
+        }
+      : {
+          '*': {
+            '*': ['storageLayout'], // This is needed by Smock for mocking functions
+          },
+        },
+  },
+}
 
 const config = defaultConfig({
   solidityVersion: '0.8.17',
+  solidityOverrides: {
+    'contracts/BalancedVault.sol': MINIMUM_CONTRACT_SIZE_SOLIDITY_OVERRIDES,
+  },
   externalDeployments: {
     goerli: [`${eqPerennialDir}/deployments/goerli`, `${eqPerennialDir}/external/deployments/goerli`],
     mainnet: [`${eqPerennialDir}/deployments/mainnet`, `${eqPerennialDir}/external/deployments/mainnet`],
