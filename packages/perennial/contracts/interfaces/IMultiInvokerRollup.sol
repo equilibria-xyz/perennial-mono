f3f0f568f41a6ba478b493626d242806e47e67ca
Kevin Britz <kevin@equilibria.xyz>
2023-04-01 19:53:20 -0700

    final sc cleanup

--- packages/perennial/contracts/interfaces/IMultiInvokerRollup.sol
+++ packages/perennial/contracts/interfaces/IMultiInvokerRollup.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.13;
 import "./IMultiInvoker.sol";
 
 interface IMultiInvokerRollup is IMultiInvoker {
-    event AddressAddedToCache(address indexed addr, uint256 nonce);
-    /// @dev reverts when calldata has an issue. causes: length of bytes in a uint > || cache index empty
-    error MultiInvokerRollupInvalidCalldataError();
+    struct PTR {
+        uint256 pos;
+    }
 
-    function addressCache(uint256 nonce) external view returns(address);
-    function addressLookup(address addr) external view returns(uint256 nonce);
+    event AddressAddedToCache(address indexed value, uint256 index);
+
+    error MultiInvokerRollupAddressIndexOutOfBoundsError();
+    error MultiInvokerRollupInvalidUint256LengthError();
+
+    function addressCache(uint256 index) external view returns(address);
+    function addressLookup(address value) external view returns(uint256 index);
 }
